478cf83a2b40d369a04b77bf0a54d6df6b5739a8
GabrielScalici <gabrielhc.scalici@gmail.com>
2018-07-13 14:12:07 -0300

    caixa caindo

--- Exs/Primeira_Fisica/sketch.js
+++ Exs/Primeira_Fisica/sketch.js
@@ -1,51 +1,23 @@
+// module aliases
+var Engine = Matter.Engine,
+    //Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
 
-var circle = {
-    x: 0,
-    y: 0,
-}
-
-var color = {
-    r: 0,
-    g: 0,
-    b: 0,
-}
-
+var engine;
+var world;
+var box1;
 
 function setup() {
-  // put setup code here
     createCanvas(1200,640);
-    background(255);
+    engine = Engine.create();
+    world = engine.world;
+    Engine.run(engine);
+    box1 = new Box(200, 100, 50, 50)
+
 }
 
 function draw() {
-
-    color.r = random(100,255);
-    color.b = random(100,200);
-    color.g = 0;
-
-    circle.x = random(0, 1200);
-    circle.y = random(0, 640);
-
-    noStroke();
-    fill(color.r,color.g,color.b,100);
-    ellipse(circle.x,circle.y,24,24);
-
-}
-
-function mouseDragged() {
-    color.r = random(100,105);
-    color.b = random(100,105);
-    color.g = 0;
-
-    noStroke();
-    fill(color.r,color.g,color.b,100);
-    ellipse(mouseX,mouseY, 50, 50);
-}
-
-function keyPressed() {
-    if (keyCode === LEFT_ARROW) {
-        background(255);
-    }else if (keyCode === RIGHT_ARROW) {
-        background(0);
-    }
+    background(255);
+    box1.show();
 }
